bbc615ae36ab4a82b5c2ab017a9fc8dcae7e3e45
Muhammad Maarij <muhammadmaarij7@gmail.com>
2023-10-05 01:16:26 +0500

    first commit

--- server/index.ts
+++ server/index.ts
@@ -46,12 +46,12 @@ app.use(cors());
 //   })
 // );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 // app.use(cors(options));
 app.use(cookieParser(process.env.COOKIE_SECRET as string));
 app.use("/users", userRouter);
