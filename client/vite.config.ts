43db1eff0d85903f3ff6aff82383c9491ff00459
Muhammad Maarij <muhammadmaarij7@gmail.com>
2023-10-04 23:59:41 +0500

    first commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "13.50.214.44",
+    port: 3000,
+  },
+});
